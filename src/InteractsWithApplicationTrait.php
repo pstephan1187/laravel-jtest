2f62ebea3b2c8afc3c3e1041cf4302b3c85c4fbd
Patrick Stephan <pstephan1187@gmail.com>
2016-11-15 16:45:52 -0600

    Complete rewrite.

---
+++ src/InteractsWithApplicationTrait.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace JTest;
+
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+
+trait InteractsWithApplicationTrait
+{
+	public function visit($uri)
+	{
+	    $uri = $this->baseUrl.$uri;
+
+	    if(!$this->session){
+	    	$this->newSession();
+	    }
+	    
+	    $this->resizeWindow();
+
+	    $this->session->get($uri);
+
+	    return $this;
+	}
+
+	public function seePageIs($url)
+	{
+		$currentUri = str_replace($this->baseUrl, '', $this->session->getCurrentUrl());
+		$this->assertEquals($url, $currentUri);
+
+		return $this;
+	}
+
+	public function type($text, $selector)
+	{
+		$element = $this->getElementBySelector($selector);
+		$element->sendKeys($text);
+
+		return $this;
+	}
+
+	public function press($selector)
+	{
+		$element = $this->getElementBySelector($selector);
+		$element->click();
+
+		return $this;
+	}
+
+	public function click($text)
+	{
+		$element = $this->session->findElement(WebDriverBy::linkText($text));
+		$element->click();
+
+		return $this;
+	}
+
+	public function select($value, $selector)
+	{
+
+		$element = $this->getElementBySelector($selector);
+		$options = $element->findElements(WebDriverBy::tagName('option'));
+
+		foreach($options as $option){
+			if($option->getText() == $value || $option->getAttribute('value') == $value){
+				$option->click();
+			}
+		}
+
+		return $this;
+	}
+
+	public function submit($selector)
+	{
+		$this->getElementBySelector($selector)->submit();
+
+		return $this;
+	}
+
+	public function see($text)
+	{
+		$body_text = $this->getElementBySelector('body')->getText();
+
+		$this->assertContains($text, $body_text);
+
+		return $this;
+	}
+
+	public function element($selector)
+	{
+		return $this->getElementBySelector($selector);
+	}
+
+	public function elements($selector)
+	{
+		return $this->getElementsBySelector($selector);
+	}
+
+	public function wait($seconds)
+	{
+		sleep($seconds);
+
+		return $this;
+	}
+
+	public function waitUntilElementExists($selector, $timeout = 5, $interval = 200)
+	{
+		return $this->waitUntil(
+			WebDriverExpectedCondition::presenceOfElementLocated(
+				WebDriverBy::cssSelector($selector)
+			)
+		);
+	}
+
+	public function waitUntilText($text, $timeout = 5, $interval = 200)
+	{
+		return $this->waitUntil(
+			WebDriverExpectedCondition::textToBePresentInElement(
+				WebDriverBy::tagName('body'),
+				$text
+			)
+		);
+	}
+
+	public function waitUntil($until, $timeout = 5, $interval = 200)
+	{
+		$this->session->wait($timeout, $interval)->until($until);
+
+		return $this;
+	}
+
+	protected function getElementBySelector($selector)
+	{
+		return $this->session->findElement(WebDriverBy::cssSelector($selector));
+	}
+
+	protected function getElementsBySelector($selector)
+	{
+		return $this->session->findElements(WebDriverBy::cssSelector($selector));
+	}
+}
--- src/JTestCase.php
+++ src/JTestCase.php
@@ -2,62 +2,108 @@
 
 namespace JTest;
 use Illuminate\Foundation\Testing\ApplicationTrait;
-use Illuminate\Foundation\Testing\AssertionsTrait;
-use Illuminate\Foundation\Testing\CrawlerTrait;
+// use Illuminate\Foundation\Testing\AssertionsTrait;
+// use Illuminate\Foundation\Testing\CrawlerTrait;
 
-class JTestCase extends \PHPUnit_Extensions_Selenium2TestCase
+use Facebook\WebDriver\WebDriverDimension;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+
+class JTestCase extends \PHPUnit_Framework_TestCase
 {
-	use ApplicationTrait, AssertionsTrait, CrawlerTrait;
+	use ApplicationTrait,
+		// AssertionsTrait,
+		// CrawlerTrait,
+		InteractsWithApplicationTrait;
 
 	protected $browser = 'chrome';
 	protected $baseUrl = 'http://localhost:8000';
 	protected $seleniumPort = 4444;
     protected $beforeApplicationDestroyedCallbacks = [];
 
+	protected $session;
+	protected $browser_width;
+	protected $browser_height;
+
+	public function setUp()
+	{
+	    parent::setUp();
+	    $this->refreshApplication();
+	}
+
     public function createApplication()
     {
-        $app = require base_path('bootstrap/app.php');
+        $app = require(realpath('./bootstrap/app.php'));
 
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
     }
 
-    public function setup()
-    {
-        $this->setBrowser($this->browser);
-        $this->setBrowserUrl($this->baseUrl);
-        $this->setPort($this->seleniumPort);
+	public function closeBrowser()
+	{
+	    if ($this->session) {
+	        $this->session->quit();
+	    }
+	}
 
-        if (! $this->app) {
-            $this->refreshApplication();
-        }
-    }
-    
-    protected function beforeApplicationDestroyed(callable $callback)
-    {
-        $this->beforeApplicationDestroyedCallbacks[] = $callback;
-    }
+	public function newSession()
+	{
+		$host = 'http://localhost:'.$this->seleniumPort.'/wd/hub';
+		$browser = $this->browser;
 
-    public function tearDown()
-    {
-        if ($this->app) {
-            foreach ($this->beforeApplicationDestroyedCallbacks as $callback) {
-                call_user_func($callback);
-            }
+		$this->session = RemoteWebDriver::create(
+			$host,
+			DesiredCapabilities::$browser()
+		);
 
-            $this->app->flush();
+		return $this->session;
+	}
 
-            $this->app = null;
-        }
+	public function setWindowSize($width, $height)
+	{
+		$this->browser_width = $width;
+		$this->browser_height = $height;
 
-        if (property_exists($this, 'serverVariables')) {
-            $this->serverVariables = [];
-        }
+		$this->resizeWindow();
 
-        if (class_exists('Mockery')) {
-            Mockery::close();
-        }
-    }
+		return $this;
+	}
 
+	protected function resizeWindow()
+	{
+		if($this->browser_width && $this->browser_height && $this->session){
+			$this->session->manage()->window()->setSize(
+				new WebDriverDimension($this->browser_width, $this->browser_height)
+			);
+		}
+	}
+
+	protected function beforeApplicationDestroyed(callable $callback)
+	{
+	    $this->beforeApplicationDestroyedCallbacks[] = $callback;
+	}
+
+	public function tearDown()
+	{
+		$this->closeBrowser();
+
+		if ($this->app) {
+		    foreach ($this->beforeApplicationDestroyedCallbacks as $callback) {
+		        call_user_func($callback);
+		    }
+
+		    $this->app->flush();
+
+		    $this->app = null;
+		}
+
+		if (property_exists($this, 'serverVariables')) {
+		    $this->serverVariables = [];
+		}
+
+		if (class_exists('Mockery')) {
+		    \Mockery::close();
+		}
+	}
 }
